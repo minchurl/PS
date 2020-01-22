1828ce39046c4d25ade195e91ba34dd92e822167
minchurl <minchurl001@gmail.com>
2020-01-22 23:24:01 +0900

    add template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define MAX_N 3005
+#define MAX_M 1000005
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define all(V) (V).begin(),(V).end()
+#define reset(V) (V).clear();(V).resize(0);
+#define sq(x) ((x)*(x))
+#define abs(x) ((x)>0?(x):(-(x)))
+#define fi first
+#define se second
+#define LL_inf 40000000000000000LL
+#define full_inf 0x7fffffff
+#define half_inf 0x3fffffff
+#define inf 0x3fffffff
+#define MOD 1000000007LL
+#define cpx_mod(x) (((LD)(((LL)x.real())%MOD)),((LD)(((LL)x.imag())%MOD)))
+using namespace std;
+typedef long long LL;
+typedef long double LD;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pil;
+typedef pair<LL,string> pls;
+typedef complex<LD> Complex;
+typedef long double LD;
+int main(){
+	freopen("input.txt","r",stdin);
+	return 0;
+}
